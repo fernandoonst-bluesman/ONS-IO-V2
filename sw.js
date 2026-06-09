d309a95364f0ed54ee36e2d8df9d11568c0bb5e9
fernandoonst-bluesman <fernandoonst@gmail.com>
2026-06-08 23:17:42 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,33 +1,44 @@
-const CACHE_NAME = 'ons-odonto-v1';
-const OFFLINE_URL = '/offline.html';
+const CACHE_NAME = 'ons-io-v1';
+const urlsToCache = [
+  '/ONS-IO-V2/',
+  '/ONS-IO-V2/index.html'
+];
 
 self.addEventListener('install', event => {
+  console.log('Service Worker: Instalando...');
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        '/offline.html',
-        '/manifest.json',
-        'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
-        'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&family=Barlow+Condensed:wght@600;700&display=swap'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Service Worker: Cache aberto');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
 self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate') {
-    event.respondWith(
-      fetch(event.request).catch(() => {
-        return caches.match(OFFLINE_URL);
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
       })
-    );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  }
+  );
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker: Ativado');
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cache => {
+          if (cache !== CACHE_NAME) {
+            console.log('Service Worker: Removendo cache antigo', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
 });
